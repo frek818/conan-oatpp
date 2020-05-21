eb07569c4458dbd04ca7555a6855410ac4f62252
Herman Griffin <Herman.Griffin@protonmail.com>
2020-05-21 21:54:30 +0800

    fix conan commands

--- conanfile.py
+++ conanfile.py
@@ -43,6 +43,7 @@ class OatppConan(ConanFile):
 
         self._cmake = CMake(self)
         self._cmake.definitions["OATPP_BUILD_TESTS"] = False
+        self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
